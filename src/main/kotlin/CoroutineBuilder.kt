4b1f5d8b7c2fef8ba08e204a0cead0c9467342e6
Sapehiasahil1 <officialsahilsapehia03@gmail.com>
2023-10-07 17:04:04 +0530

    Done launch

---
+++ src/main/kotlin/CoroutineBuilder.kt
@@ -0,0 +1,40 @@
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
+
+/*
+    Coroutine Builders -> They create the coroutines
+
+    1. launch : It is also called "FIRE AND FORGET" meaning when launch is called a new coroutine is
+        created and it will not return anything to its caller.
+
+    Diff b/w launch and GlobalScope.launch ?
+        ## launch -> It has a local scope which means that the coroutine will destroy as soon as the component
+        (activity) of the app gets destroyed.
+        -> It runs on its immediate parent thread.
+        eg :- login etc.
+
+        ## GlobalScope.launch -> It has a global scope which means it can survive to the entire life of the
+        application.
+        eg :- file download etc.
+
+    2. runBlocking
+    3. async
+*/
+
+fun main() = runBlocking {
+
+    println("Main program starts : ${Thread.currentThread().name}")
+
+    val job : Job = launch {// Thread : Main
+        println("Fake work starts : ${Thread.currentThread().name}") // Thread : Main
+        delay(1000) // Coroutine is suspended and main thread is free (not blocked)
+        println("Fake work ends : ${Thread.currentThread().name}") // Thread : Main or different thread
+
+    }
+
+    job.join() // wait for the coroutine to complete the task.
+    //delay(2000)
+    println("Main program ends: ${Thread.currentThread().name}") // Thread : Main
+}
